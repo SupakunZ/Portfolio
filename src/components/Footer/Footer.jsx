13b044b61cd9aa6b2316686f74e75f791eb2d777
Supakun Thata <supakunt.thata@gmail.com>
2024-07-11 17:04:40 +0700

    First version

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import { IoStar } from "react-icons/io5";
 function Footer() {
   return (
     <div className="relative border-t bg-[#0b1020] border-[#353951] text-white">
-      <div className="mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] py-5 lg:py-9">
+      <div className="mx-auto px-6 sm:px-20 lg:max-w-[60rem] xl:max-w-[76rem] 2xl:max-w-[80rem] py-5 lg:py-9">
         <div className="flex justify-center -z-40">
           <div className="absolute top-0 h-[1px] w-1/2  bg-gradient-to-r from-transparent via-violet-500 to-transparent"></div>
         </div>
@@ -12,23 +12,15 @@ function Footer() {
           <p className="text-sm">
             © Developer Portfolio by <a target="_blank" href="https://github.com/SupakunZ" className="text-[#7000ff]">Supakun Thata</a>
           </p>
-          <div className="flex items-center gap-5">
-            <a
+          <div className="flex items-center gap-2">
+            {/* <a
               target="_blank"
               href="https://github.com/SupakunZ/Portfolio"
-              className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
+              className="flex items-center gap-2 [&>:nth-child(1)]:hover:text-[#fde047] text-sm"
             >
               <IoStar />
               <span>Star</span>
-            </a>
-            <a
-              target="_blank"
-              href="https://github.com/said7388/developer-portfolio/fork"
-              className="flex items-center gap-2 uppercase hover:text-[#16f2b3]"
-            >
-              <CgGitFork />
-              <span>Fork</span>
-            </a>
+            </a> */}
           </div>
         </div>
       </div>
