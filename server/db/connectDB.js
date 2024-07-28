053e56f7b8a6d21825b2dc104a776d7c67ecbdae
Supakun Thata <supakunt.thata@gmail.com>
2024-07-28 16:26:10 +0700

    add contact

--- server/db/connectDB.js
+++ server/db/connectDB.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv').config()
 
+
 module.exports = async (req, res) => {
   try {
     await mongoose.connect(process.env.MONGO_URL)
